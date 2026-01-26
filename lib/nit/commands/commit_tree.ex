0f7569592d8ea0a276dae08e3baf9c37f8229a9a
Nicolas Guerrero <nicolasguerrerodev@gmail.com>
2026-01-26 20:46:34 -0300

    update the commit-tree command to update the last commit on the branch

--- lib/nit/commands/commit_tree.ex
+++ lib/nit/commands/commit_tree.ex
@@ -40,7 +40,29 @@ defmodule Nit.Commands.CommitTree do
     compressed = :zlib.compress(store)
 
     ObjectStore.save_object_on_disk(sha_hex, compressed)
+    update_head_branch_with_latest_commit(sha_hex)
 
     sha_hex
+  end
+
+  defp update_head_branch_with_latest_commit(latest_commit_sha) do
+    current_branch_path = get_current_branch_path()
+
+    branch_dir =
+      current_branch_path
+      |> Path.dirname()
+
+    File.mkdir_p!(branch_dir)
+    File.write!(current_branch_path, "#{latest_commit_sha}\n")
+
+    {:ok}
+  end
+
+  defp get_current_branch_path() do
+    ".nit/HEAD"
+    |> File.read!()
+    |> String.trim()
+    |> String.replace_prefix("ref: ", "")
+    |> then(&Path.join(".nit", &1))
   end
 end
